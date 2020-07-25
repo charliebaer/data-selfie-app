a13cadb8a8bc91093061a7c496971a9bc0618719
charliebaer <sanjaypaulcharles98@gmail.com>
2020-07-26 03:09:09 +0530

    added db feature

--- public/index.js
+++ public/index.js
@@ -1,30 +1,34 @@
 
 
-if ('geolocation' in navigator) {
-    console.log('Geolocation API available');
-    navigator.geolocation.getCurrentPosition(async (position) => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
+document.getElementById('geolocate').addEventListener('click', () => {
+    if ('geolocation' in navigator) {
+        console.log('Geolocation API available');
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
 
-        document.getElementById('latitude').textContent = lat;
+            document.getElementById('latitude').textContent = lat;
 
-        document.getElementById('longitude').textContent = lon;
+            document.getElementById('longitude').textContent = lon;
 
-        const data = { lat, lon };
-        const option = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
+            const data = { lat, lon };
+            const option = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
 
-        };
+            };
 
-        const response = await fetch('/api', option);
-        const json = await response.json();
-        console.log(json);
-    });
-}
-else {
-    console.log('not available');
-}
+            const response = await fetch('/api', option);
+            const json = await response.json();
+            console.log(json);
+        });
+    }
+    else {
+        console.log('not available');
+    }
+});
+
+
--- server.js
+++ server.js
@@ -1,22 +1,7 @@
 
-/*
 const express = require('express');
 
-const app = express();
-
-app.listen(3000, () => console.log("listening at port 3000"));
-
-app.use(express.static('public'));
-
-app.use(express.json({ limit: '2mb' }));
-
-app.post('/api', (request, response) => {
-    console.log('request recieved!');
-    console.log(typeof (response.body));
-}) 
-*/
-const express = require('express');
-
+const datastore = require('nedb');
 
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
@@ -24,15 +9,21 @@ app.use(express.static('public'));
 
 app.use(express.json());
 
+const database = new datastore('database.db');
+database.loadDatabase();
 
+database.insert({
+    name : "Charles",
+    status : "dufus"
+});
 app.post('/api', (request, response) => {
     console.log('request received');
     console.log((request.body));
     const data = request.body;
     response.json({
-        status : 'success',
-        latitude : data.lat,
-        longitude : data.lon
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
     })
 
 });
